37d6e64a1d3c2eac3198a2b9e71bc2884fd67437
Wanderley <junior.wander@gmail.com>
2020-08-24 15:11:41 -0300

    Criacao da classe pessoa

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,11 +1,21 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35, sexo='m'):
+        self.nome = nome
+        self.idade = idade
+        self.sexo = sexo
+
     def comprimentar(self):
         return f'Olá {id(self)}'
 
 
 if __name__ == '__main__':
-     p = Pessoa()
-
+     p = Pessoa('wanderley')
      print(Pessoa.comprimentar(p))
      print(id(p))
      print(p.comprimentar())
+     p.nome = 'wanderley'
+     print(p.nome)
+     p.idade = 34
+     print(p.idade)
+     p.sexo = 'M'
+     print(p.sexo)
